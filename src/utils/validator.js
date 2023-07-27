d041cb138e2ac5de0f3e24045ba04fa480688bb8
Illia <illia.vasiliev@gmail.com>
2023-07-28 01:25:10 +0300

    fix validator

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -4,34 +4,21 @@ import { ethers } from 'ethers';
 export const validateAddress = address => {
   const addressRegex = /^0x[0-9a-fA-F]{40}$/;
 
-  if (addressRegex.test(address)) {
-    if (ethers.utils.isAddress(address)) {
-      return true;
-    } else {
-      toast.error('The entered address is not a valid Ethereum address.');
-    }
-
-    return false;
-  } else {
+  if (!addressRegex.test(address)) {
     toast.error('Invalid Ethereum address format.');
     return false;
   }
+
+  if (!ethers.utils.isAddress(address)) {
+    toast.error('The entered address is not a valid Ethereum address.');
+    return false;
+  }
+
+  return true;
 };
 
 export const validateTransfer = amount => {
-  let isValidAmount = false;
-  const amountWithoutZero = (amount * 10) / 10;
-  const amountWithoutZeroString = amountWithoutZero.toString();
-  const amountLengthAfterDote = amountWithoutZero.toString().split('.')[1]?.length;
-
-  if (amountLengthAfterDote) {
-    isValidAmount = amountWithoutZeroString[amountWithoutZeroString.length - 1] === '1';
-  } else if (amount === '1') {
-    isValidAmount = true;
-  } else {
-    isValidAmount = amount % 10 === 0;
-  }
-
+  const isValidAmount = amount % 10 === 0 || /^0\./.test(amount);
   const isInRange = parseFloat(amount) >= 0.000001 && parseFloat(amount) <= 100000;
 
   if (isValidAmount && isInRange) {
